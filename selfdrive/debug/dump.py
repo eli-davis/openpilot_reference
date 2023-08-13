3ad55ebf834c2e2603698bc3c334b5a56d5059b7
Aarav Pandya <ap7641@nyu.edu>
2023-08-13 13:10:33 -0400

    Can Replay

--- selfdrive/debug/dump.py
+++ selfdrive/debug/dump.py
@@ -6,21 +6,17 @@ import json
 from hexdump import hexdump
 import codecs
 codecs.register_error("strict", codecs.backslashreplace_errors)
-
+import pandas as pd
 from cereal import log
 import cereal.messaging as messaging
 from cereal.services import service_list
+import signal
+import sys
 
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(description='Dump communication sockets. See cereal/services.py for a complete list of available sockets.')
-  parser.add_argument('--pipe', action='store_true')
-  parser.add_argument('--raw', action='store_true')
-  parser.add_argument('--json', action='store_true')
-  parser.add_argument('--dump-json', action='store_true')
-  parser.add_argument('--no-print', action='store_true')
   parser.add_argument('--addr', default='127.0.0.1')
-  parser.add_argument('--values', help='values to monitor (instead of entire event)')
   parser.add_argument("socket", type=str, nargs='*', help="socket names to dump. defaults to all services defined in cereal")
   args = parser.parse_args()
 
@@ -29,83 +25,49 @@ if __name__ == "__main__":
     messaging.context = messaging.Context()
 
   poller = messaging.Poller()
-
+  data_list = []
   for m in args.socket if len(args.socket) > 0 else service_list:
     messaging.sub_sock(m, poller, addr=args.addr)
-
+  def signal_handler(sig, frame):
+    print('KeyboardInterrupt detected, writing DataFrame to Parquet file...')
+    df = pd.DataFrame(data_list)
+    df.to_parquet('candump.parquet')
+    sys.exit(0)
+  signal.signal(signal.SIGINT, signal_handler)
   values = None
-  if args.values:
-    values = [s.strip().split(".") for s in args.values.split(",")]
-  with open('output.txt', 'a') as f:
+  with open('candump.txt', 'a') as f:
     while 1:
       polld = poller.poll(100)
       for sock in polld:
         msg = sock.receive()
-        evt = log.Event.from_bytes(msg)
 
-<<<<<<< Updated upstream
-  while 1:
-    polld = poller.poll(100)
-    for sock in polld:
-      msg = sock.receive()
-      with log.Event.from_bytes(msg) as log_evt:
-        evt = log_evt
-
-      if not args.no_print:
-        if args.pipe:
-          sys.stdout.write(msg)
-          sys.stdout.flush()
-        elif args.raw:
-          hexdump(msg)
-        elif args.json:
-          print(json.loads(msg))
-        elif args.dump_json:
-          print(json.dumps(evt.to_dict()))
-        elif values:
-          print(f"logMonotime = {evt.logMonoTime}")
-          for value in values:
-            if hasattr(evt, value[0]):
-              item = evt
-              for key in value:
-                item = getattr(item, key)
-              print(f"{'.'.join(value)} = {item}")
-          print("")
-        else:
-          try:
-            print(evt)
-          except UnicodeDecodeError:
-            w = evt.which()
-            s = f"( logMonoTime {evt.logMonoTime} \n  {w} = "
-            s += str(evt.__getattr__(w))
-            s += f"\n  valid = {evt.valid} )"
-            print(s)
-=======
-        if not args.no_print:
-          if args.pipe:
-            sys.stdout.write(msg)
-            sys.stdout.flush()
-          elif args.raw:
-            hexdump(msg)
-          elif args.json:
-            print(json.loads(msg))
-          elif args.dump_json:
-            print(json.dumps(evt.to_dict()))
-          elif values:
-            print(f"logMonotime = {evt.logMonoTime}")
-            for value in values:
-              if hasattr(evt, value[0]):
-                item = evt
-                for key in value:
-                  item = getattr(item, key)
-                print(f"{'.'.join(value)} = {item}")
-            print("")
-          else:
-            try:
-              f.write(str(evt))
-            except UnicodeDecodeError:
-              w = evt.which()
-              s = f"( logMonoTime {evt.logMonoTime} \n  {w} = "
-              s += str(evt.__getattr__(w))
-              s += f"\n  valid = {evt.valid} )"
-              print(s)
->>>>>>> Stashed changes
+        with log.Event.from_bytes(msg) as log_evt:
+          evt = log_evt
+  
+        try:
+           if evt.which() == 'can':
+                    # Parse the log.Event into a dictionary
+                    data = {
+                        'logMonoTime': evt.logMonoTime,
+                        'valid': evt.valid
+                    }
+                    for can_msg in evt.can:
+                      data = {
+                          'logMonoTime': evt.logMonoTime,
+                          'valid': evt.valid,
+                          'can_address': can_msg.address,
+                          'can_busTime': can_msg.busTime,
+                          'can_dat': can_msg.dat,
+                          'can_src': can_msg.src
+                      }
+                      data_list.append(data)
+                    # Add the dictionary to data_list
+                      
+        except UnicodeDecodeError:
+          w = evt.which()
+          s = f"( logMonoTime {evt.logMonoTime} \n  {w} = "
+          s += str(evt.__getattr__(w))
+          s += f"\n  valid = {evt.valid} )"
+          print(s)
+df = pd.DataFrame(data_list)
+df.to_parquet('candump.parquet')
